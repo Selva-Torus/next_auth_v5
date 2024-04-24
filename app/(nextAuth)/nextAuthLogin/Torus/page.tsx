2d132785ba29e3f596f2aa769de1cebcc27be0bd
hari6565 <harin@torus.tech>
2024-04-24 15:22:48 +0530

    new 4

--- app/(nextAuth)/nextAuthLogin/Torus/page.tsx
+++ app/(nextAuth)/nextAuthLogin/Torus/page.tsx
@@ -1,10 +1,52 @@
+"use client";
 import Torus from "@/app/Torus/Torus";
-import React from "react";
+import {
+  checkIsActive,
+  logoutRealm,
+} from "@/app/utilsFunctions/ulits/keyCloakAuth";
+import { signOut, useSession } from "next-auth/react";
+import React, { useEffect } from "react";
 
 const Home = () => {
+  const { status, data: session } = useSession();
+  useEffect(() => {
+    const allDeatiles = JSON.parse(JSON.stringify(session));
+    console.log(allDeatiles, "allDeatiles");
+    if (allDeatiles.hasOwnProperty("user")) {
+      var client = {
+        realm: allDeatiles.user.realm,
+        client_id: allDeatiles.user.client_id,
+        client_secret: allDeatiles.user.client_secret,
+      };
+      checkIsActive(client, allDeatiles.user.token).then((res) => {
+        console.log(res.active);
+        if (!res.active) {
+          signOut();
+        }
+      });
+    }
+  }, []);
+  async function Logout() {
+    const allDeatiles = JSON.parse(JSON.stringify(session));
+    console.log(allDeatiles, "allDeatiles");
+
+    if (allDeatiles.user) {
+      var demo = {
+        realm: allDeatiles.user.realm,
+        client_id: allDeatiles.user.client_id,
+        client_secret: allDeatiles.user.client_secret,
+      };
+      const res = await logoutRealm(demo, allDeatiles.user.token);
+      signOut();
+    } else {
+      alert("pls wait to load");
+    }
+    return;
+  }
+
   return (
     <div>
-      <Torus />
+      <Torus session={JSON.stringify(session)} Logout={Logout} />
     </div>
   );
 };
--- app/Torus/Torus.tsx
+++ app/Torus/Torus.tsx
@@ -1,39 +1,12 @@
 "use client";
-import { signOut, useSession } from "next-auth/react";
+
 import React from "react";
-import { logoutRealm } from "../utilsFunctions/ulits/keyCloakAuth";
 
-const Torus = () => {
-  const { status, data: session } = useSession();
-  async function Logout() {
-    const allDeatiles = JSON.parse(JSON.stringify(session));
-    console.log(allDeatiles, "allDeatiles");
-
-    if (allDeatiles.status == "authenticated") {
-      if (allDeatiles.data.user) {
-        var demo = {
-          realm: allDeatiles.data.user.realm,
-          client_id: allDeatiles.data.user.client_id,
-          client_secret: allDeatiles.data.user.client_secret,
-        };
-        const res = await logoutRealm(demo, allDeatiles.data.user.token);
-        signOut();
-      }
-    } else {
-      alert("Loaing out failed");
-    }
-
-    return;
-
-    // const res = await logoutRealm(data, token);
-    // localStorage.removeItem("token");
-    // localStorage.removeItem("user");
-  }
-
+const Torus = ({ session, Logout }: any) => {
   return (
     <>
-      <div>page</div>
-      <div>{JSON.stringify(session)}</div>
+      <div>page success</div>
+      <div>{session}</div>
       <button onClick={Logout}>Signout</button>
     </>
   );
